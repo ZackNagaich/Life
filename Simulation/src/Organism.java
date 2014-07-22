9240472d2b73c27c69e178f236640c232b89ce1e
root <root@localhost.localdomain>
2014-07-21 21:04:00 -0400

    Keyboard input added for control of initial seed conditions. Multiple species defined

--- Simulation/src/Organism.java
+++ Simulation/src/Organism.java
@@ -5,11 +5,13 @@ public class Organism {
 	int x = 0;
 	int y = 0;
 	boolean isAlive = false;
+        int species = 0;
 	
-	public Organism(int x, int y,int fate){
+	public Organism(int x, int y, int fate, int species){
 		this.x = x;
 		this.y = y;
-		
+		this.species = species;
+                
 		if(fate == 1){
 			this.isAlive = true;
 		}
@@ -22,9 +24,11 @@ public class Organism {
 	public void die() {
 		isAlive = false;
 	}
-	
+        
+        
 	public void bringToLife() {
 		isAlive = true;
 	}
 
 }
+
--- Simulation/src/World.java
+++ Simulation/src/World.java
@@ -1,8 +1,7 @@
-	 
 import java.awt.*;
-
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
-
 import javax.swing.*;
 	 
 
@@ -11,28 +10,83 @@ import javax.swing.*;
 
 		private static final long serialVersionUID = 1L;
 		
+                private static boolean runSimulation = true;
+                private static int worldWidth = 800;
+                private static int worldHeight = 600;
 		private static int width = 2;
 		private static int height = 2;
-		private static int arcWidth =2;
-		private static int arcHeight = 2;
-		private static Organism[][] worldContainer = new Organism[800][600];
-		
+		private static int arcWidth = 1;
+		private static int arcHeight = 1;
+		private static Organism[][] worldContainer = new Organism[worldWidth][worldHeight];
+                private static World world = new World();
+                
+                public World(){
+                        addKeyListener(new KeyListener() {
+                            
+                            @Override
+                            public void keyTyped(KeyEvent e) {
+                            }
+
+                            @Override
+                            public void keyReleased(KeyEvent e) {
+                                
+                            }
+
+                            @Override
+                            public void keyPressed(KeyEvent e) {
+                                if (e.getKeyCode() == KeyEvent.VK_SPACE){
+
+                                    if(runSimulation == true){
+                                        
+                                         runSimulation = false;
+                                    }
+                                    
+                                    else{
+                                        
+                                        runSimulation = true;
+                                    }
+                                }
+                                
+                                if (e.getKeyCode() == KeyEvent.VK_R){
+
+                                    generateRandomOrganisms();
+                                }
+                                
+                                if (e.getKeyCode() == KeyEvent.VK_C){
+
+                                    generateCenterOrganisms();
+                                }
+                                
+                                if (e.getKeyCode() == KeyEvent.VK_S){
+
+                                    generateSidedOrganisms();
+                                }
+                                                                   
+                            }
+                    });
+                    setFocusable(true);
+                }
+                
 		private void doDrawing(Graphics g) {
 
-	        Graphics2D g2d = (Graphics2D) g;
+                    Graphics2D g2d = (Graphics2D) g;
 	       
-	        for (int i = 0; i < 800; i++) {
-	    		 for(int j = 0; j < 600; j++){
-	    			 
-		        		Organism organism = worldContainer[i][j];
-		        		if(organism != null){
-		        			if(organism.isAlive){
-		        					g2d.setColor(Color.blue);
-		        					g2d.fillRoundRect(organism.x, organism.y, width, height, arcWidth, arcHeight);
-		        			}
-		        		}
-	    		 }
-	        }
+                    for (int i = 0; i < worldWidth; i++) {
+                    	 for(int j = 0; j < worldHeight; j++){
+                            Organism organism = worldContainer[i][j];
+                            if(organism != null){
+                            	if(organism.isAlive){
+                                    if(organism.species == 1){
+                                        g2d.setColor(Color.green);
+                                    }
+                                    else if(organism.species == 0){
+                                        g2d.setColor(Color.green);
+                                    }
+                                    g2d.fillRoundRect(organism.x, organism.y, width, height, arcWidth, arcHeight);
+		        	}
+                            }
+                        }
+                    }
 	    }
 
 	    @Override
@@ -45,54 +99,100 @@ import javax.swing.*;
 		
 	    public static void main(String s[]) {
 	       
-	    	World world = new World();
+                world.setBackground(Color.black);
 	    	JFrame frame = new JFrame("Simulation");
 	    	frame.add(world);
-	    	frame.setSize(800,600);
+	    	frame.setSize(worldWidth,worldHeight);
 	    	frame.setVisible(true);
 	    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    	
 
-	    	generateOrganisms();
+	    	generateRandomOrganisms();
 
 	    	
 	    	world.repaint();
 	    	
 	    	while(true){
 	    		
-	    		try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
+                    if(runSimulation){
+     
 	    		updatePopulation();
-	    		
-	    		
+	    	
 	    		world.repaint();
+                    }
 	    	}
 	    }
 	    
 	    
-	    public static void generateOrganisms(){
+	    public static void generateRandomOrganisms(){
 	    	
-	    	 for (int i = 0; i < 800; i++) {
-	    		 for(int j = 0; j < 600; j++){
-	    			 
-	    		  Random r = new Random();
+                Random r = new Random();
+                
+	    	 for (int i = 0; i < worldWidth; i++) {
+	    		 for(int j = 0; j < worldHeight; j++){
 	    		  
-	    		  int fate = r.nextInt() % 19;
-		          worldContainer[i][j] = new Organism(i,j,fate);
+                          int species = r.nextInt() % 2;	  
+	    		  int fate = r.nextInt() % 10;
+		          worldContainer[i][j] = new Organism(i,j,fate,species);
 
 	    		 }
 		     }
 
 	    }
+            
+            public static void generateCenterOrganisms(){
+	    	
+                Random r = new Random();
+                
+                for(int i = 0; i < worldWidth; i++){
+	    		for(int j = 0; j < worldHeight; j++){
+                            
+                            int species = r.nextInt() % 2;
+                            worldContainer[i][j] = new Organism(i,j,0,species);
+                        }
+                }     
+                
+                for(int i = 400; i < 410; i++){
+	    		for(int j = 300; j < 310; j++){
+                            
+                            int species = r.nextInt() % 2;
+                            worldContainer[i][j] = new Organism(i,j,1,species);
+                        }
+                }
+
+	    }
 	    
+            
+             public static void generateSidedOrganisms(){
+
+                
+                for(int i = 0; i < worldWidth; i++){
+	    		for(int j = 0; j < worldHeight; j++){
+                            
+                            
+                            worldContainer[i][j] = new Organism(i,j,0,0);
+                        }
+                }     
+                
+                 for (int i = 398; i < 400; i++) {
+                     for (int j = 0; j < 410; j++) {
+                         worldContainer[i][j] = new Organism(i,j,1,1);
+                     }
+                 }
+
+	    }
+             
+             
 	    public static void updatePopulation(){
 	    	
-	    	for(int i = 0; i < 800; i++){
-	    		for(int j = 0; j < 600; j++){
+	    	for(int i = 0; i < worldWidth; i++){
+	    		for(int j = 0; j < worldHeight; j++){
 	    			
 		    		Organism organism = worldContainer[i][j];
 		    		
@@ -101,52 +201,106 @@ import javax.swing.*;
 		    		 */
 		    		int organX = organism.x;
 		    		int organY = organism.y;
+                                int organSpecies = organism.species;
+                                boolean oppositeSpecies = false;
 		    		
 		    		int adjacentSurvivors = 0;
 		    		
 		    		boolean left = isAlive(organX-1,organY);
+                                
+                                if(speciesAtPosition(organX-1,organY) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(left){
 		    			adjacentSurvivors++;
 		    		}
+                                
+                                
 		    		boolean right = isAlive(organX+1,organY);
+                                if(speciesAtPosition(organX+1,organY) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(right){
 		    			adjacentSurvivors++;
 		    		}
+                                
+                              
 		    		boolean upright = isAlive(organX+1,organY+1);
+                                if(speciesAtPosition(organX+1,organY+1) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(upright){
 		    			adjacentSurvivors++;
 		    		}
+                                
+                                
 		    		boolean upleft = isAlive(organX-1,organY+1);
+                                if(speciesAtPosition(organX-1,organY+1) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(upleft){
 		    			adjacentSurvivors++;
 		    		}
+                                
+                                
 		    		boolean up = isAlive(organX,organY+1);
+                                if(speciesAtPosition(organX,organY+1) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(up){
 		    			adjacentSurvivors++;
 		    		}
+                                
+                                
 		    		boolean down = isAlive(organX,organY-1);
+                                if(speciesAtPosition(organX,organY-1) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(down){
 		    			adjacentSurvivors++;
 		    		}
+                                
+                                
 		    		boolean downright = isAlive(organX+1,organY-1);
+                                if(speciesAtPosition(organX+1,organY-1) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(downright){
 		    			adjacentSurvivors++;
 		    		}
-		    	    boolean downleft = isAlive(organX-1,organY-1);
+                                
+                                
+                                boolean downleft = isAlive(organX-1,organY-1);
+                                if(speciesAtPosition(organX-1,organY-1) != organSpecies){
+                                    oppositeSpecies = true;
+                                }
 		    		if(downleft){
 		    			adjacentSurvivors++;
 		    		}
 		    		
+                                if(oppositeSpecies){
+                                    if(organism.species == 1){
+                                       // organism.species = 0;
+                                    }
+                                    else if(organism.species == 0){
+                                       // organism.species = 1;
+                                    }
+                                }
 		    		if(isAlive(organX,organY)){
 
 		    			if(adjacentSurvivors < 2 || adjacentSurvivors > 3){
-		    				organism.die();
+                                            
+                                            organism.die();
 		    			}
 		    			
 		    		}
 		    		else{
 		    			if(adjacentSurvivors == 3){
-		    				organism.bringToLife();
+                                            Random r = new Random();
+                                            if(r.nextInt() % 1 == 0){
+                                                 organism.bringToLife();
+                                            }
+                                            
 		    			}
 		    			
 		    		}
@@ -155,10 +309,20 @@ import javax.swing.*;
 	    	
 	    }
 	    
+            
+            public static int speciesAtPosition(int x, int y){
+            
+                if(x == -1 || y == -1 || x == worldWidth || y == worldHeight){
+	    		return -1;
+	    	}
+                
+                return worldContainer[x][y].species;
+            }
+	
 	    
 	    public static boolean isAlive(int x, int y){
 	    	
-	    	if(x == -1 || y == -1 || x == 800 || y == 600){
+	    	if(x == -1 || y == -1 || x == worldWidth || y == worldHeight){
 	    		return false;
 	    	}
 	    	

fcdf4dbbd52cc8a7a381ff358b4c21dc9e143b9c
DigitalDynamics <vengeance10@gmail.com>
2014-08-13 20:31:24 -0400

    Multiple species

---
+++ Simulation/src/Point.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+
+public class Point  extends JFrame{
+
+
+	public void move(String direction){
+		
+	}
+	
+	public void die(){
+		
+	}
+	
+	public void attack(String direction){
+		
+		
+	}
+	
+
+	
+}
